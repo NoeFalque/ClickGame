49415585b215016cf157220b67b3ca909de088e2
bolobolo123 <guillaume1006@hotmail.fr>
2016-12-12 16:25:03 +0100

    création des données à l'arrivée sur la page
    Only JS

--- src/js/script.js
+++ src/js/script.js
@@ -4,19 +4,19 @@ function rdm(rMin, rMax) {
 }
 
 
-
+// Character CONSTRUCTOR
 var Character = function() {
     this.phys = {};
         this.phys.hair = rdm(0,4);
         this.phys.skin = rdm(0,4);
         this.phys.body = rdm(0,4);
         this.phys.legs = rdm(0,4);
-    this.currentLife = totalLife; 
     this.totalLife = rdm(100,500);
+    this.currentLife = this.totalLife; 
     this.eaten = {};
     this.preference = rdm(1,3);
 }
-
+// Food CONSTRUCTOR
 var Food = function() {
     this.type = rdm(0,4);
     this.level = rdm(0,4);
@@ -25,3 +25,22 @@ var Food = function() {
 
 var character = new Character();
 var food = new Food();
+
+
+// LocalStorage of basic DATAS ( Coins, Upgrades, etc. )
+if (!localStorage.getItem('data')) {
+    var dataRestore = {
+        coins: 0,
+        level: 0,
+        upgrades: {
+            
+        },
+        firstGame: new Date(),
+    };
+    localStorage.setItem('data', JSON.stringify(dataRestore));
+    console.log('datas created !');
+}
+else {
+    var dataRestore = JSON.parse(localStorage.getItem('data'));
+    console.log('datas restored !');
+}
