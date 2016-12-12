ae6cffd2ab8fbc51d94556e3c76b9ddd045b83a9
bolobolo123 <guillaume1006@hotmail.fr>
2016-12-12 15:48:39 +0100

    Constructeur Personnages + Food

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,27 @@
-var character = {
-	phys: {
-		hair,
-		skin,
-		body,
-		legs,
-	},
-	life,
-	totalLife,
-	eaten: {
-		
-	},
-	preference
+// RANDOM 
+function rdm(rMin, rMax) {
+    return ~~((Math.random()*(rMax-rMin+1))+rMin);
 }
 
-var food = {
-	type,
-	level,
-	cost
+
+
+var Character = function() {
+    this.phys = {};
+        this.phys.hair = rdm(0,4);
+        this.phys.skin = rdm(0,4);
+        this.phys.body = rdm(0,4);
+        this.phys.legs = rdm(0,4);
+    this.currentLife = totalLife; 
+    this.totalLife = rdm(100,500);
+    this.eaten = {};
+    this.preference = rdm(1,3);
 }
+
+var Food = function() {
+    this.type = rdm(0,4);
+    this.level = rdm(0,4);
+    this.cost = rdm(0,4);
+}
+
+var character = new Character();
+var food = new Food();
