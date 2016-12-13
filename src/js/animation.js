09bf8ca4a5c00c5ad0c1b164103c8c7e7bdd7956
NoeFalque <noe.falque@gmail.com>
2016-12-13 17:56:02 +0100

    anim explosion + reading external svg file

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,18 +1,44 @@
 
 
-var s = Snap("#thin");
-var $fat = document.querySelector("#fat");
+//var s = Snap("#thin");
 
-var parts = ["#shirt","#jean","#skin", "#skin_body", "#neck", "#neck_shadow", "#arm_left", "#arm_right"];
 
-for(var i in parts){
-	var thin = s.select( parts[i] + "_thin");
-	var fat = document.querySelector(parts[i] + "_fat").getAttribute("d");
-	thin.node.style.visibility = "visible";
-	thin.animate({ d: fat }, 3000, mina.backout);
+var s = Snap("#svg");
+
+var man = Snap.load("man5.svg", function (f) {
+	var g = f.select("svg");
+	s.append(g);
+	fatAnim();
+	explosionAnim();
+} );
+
+
+function fatAnim(){
+	var parts = ["#shirt","#jean","#skin", "#skin_body", "#neck", "#neck_shadow", "#arm_left", "#arm_right"];
+
+	for(var i in parts){
+		var thin = s.select( parts[i] + "_thin");
+		var fat = document.querySelector(parts[i] + "_fat").getAttribute("d");
+		//	thin.node.style.visibility = "visible";
+		thin.animate({ d: fat }, 3000, mina.backout);
+	}
 }
 
-function animation(parts, el, initFlag, modif, modifFlag, callback){
+
+function explosionAnim(){
+	setTimeout(function(){
+		var parts = ["#shirt","#jean","#skin", "#skin_body", "#neck", "#neck_shadow", "#arm_left", "#arm_right", "#navel", "#shoe_top_left", "#sole_left", "#shoe_top_right", "#sole_right", "#eyebow_left", "#eyebow_right"];
+
+		for(var i in parts){
+			var fat = s.select( parts[i] + "_thin");
+			var exploded = document.querySelector(parts[i] + "_exploded").getAttribute("d");
+			fat.animate({ d: exploded }, 300, mina.easeout);
+		}
+
+	}, 3000);
+}
+
+/*function animation(parts, el, initFlag, modif, modifFlag, callback){
 	for(var i in parts){
 		el = s.select( parts[i] + initFlag);
 		modif = document.querySelector(parts[i] + modifFlag).getAttribute("d");
@@ -24,13 +50,13 @@ function animation(parts, el, initFlag, modif, modifFlag, callback){
 			callback()
 		);
 	}
-}
+}*/
 
 function eatingAnim(){
 	var parts = ["#mouth_hole", "#mouth", "#cheek_left", "#cheek_right", "#ear_right", "#ear_left", "#ear_right_hole", "#ear_left_hole"];
-	//		animation(parts, open, "_open", eating, "_eating", function(){
-	//			animation(parts, eating, "_eating", open, "_open", function(){});
-	//		});
+	/*			animation(parts, open, "_open", eating, "_eating", function(){
+				animation(parts, eating, "_eating", open, "_open", function(){});
+			});*/
 
 	function doSetTimeout(open, openPath){
 		setTimeout(function(){
@@ -48,15 +74,12 @@ function eatingAnim(){
 		var openPath = document.querySelector(parts[i] + "_open").getAttribute("d");
 
 		var open = s.select( parts[i] + "_open");
-		open.node.style.visibility = "visible";
 		open.animate(
 			{ d: eatingPath }, 
 			750, 
 			mina.easeinout
 		);
-		
 		doSetTimeout(open, openPath);
-
 	}
 }
 setInterval(function(){ eatingAnim(); }, 1500);
